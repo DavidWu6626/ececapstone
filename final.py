afbb1b64ea25635c0b92b512e8a727bedda634bd
DavidWu6626 <dswu@andrew.cmu.edu>
2024-05-01 21:09:29 -0400

    made targetting move the camera, added vertical adjusting, added location tracking

--- final.py
+++ final.py
@@ -22,6 +22,14 @@ import json
 # for laser
 import RPi.GPIO as gpio
 
+# for PTZ
+from Focuser import Focuser
+
+# for location
+from flask import Flask, jsonify
+from geopy.geocoders import Photon
+from geopy.exc import GeocoderTimedOut, GeocoderInsufficientPrivileges
+
 # general libraries
 from time import sleep
 import threading
@@ -92,12 +100,33 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
     allow_reuse_address = True
     daemon_threads = True
 
+# for streaming
+app = Flask(__name__)
+
 def doStreaming():
     picam2.start_recording(JpegEncoder(), FileOutput(output))
     address = ('', 8000)
     server = StreamingServer(address, StreamingHandler)
     server.serve_forever()        
 
+# for location
+@app.route('/location')
+def get_location():
+    try:
+        # Use geopy to retrieve geolocation based on IP address
+        geolocator = Photon(user_agent="measurements")
+        location = geolocator.geocode("172.26.165.246")
+        if location:
+            return jsonify({'latitude': location.latitude, 'longitude': location.longitude})
+        else:
+            return jsonify({'error': 'Location not found'}), 404
+    except GeocoderTimedOut:
+        return jsonify({'error': 'Geocoder service timed out'}), 500
+    except GeocoderInsufficientPrivileges:
+        return jsonify({'error': 'Insufficient privileges to access geocoding service'}), 403
+
+def doLocation():
+    app.run(host='0.0.0.0', port=5000, debug=False)
 
 # Output: True if person is found
 # Sets personDetected var to True if person is found
@@ -119,15 +148,16 @@ def checkForPerson():
 # Translates direction to JSON cmd
 def getJSONcmd(dir):
     if dir == 'L':
-        msg = {"T":1,"L":-132,"R":135} #TODO JSON COMMANDS ARE RIGHT???
+        msg = {"T":1,"L":-70,"R":70} #TODO JSON COMMANDS ARE RIGHT???
     elif dir == 'R':
-        msg = {"T":1,"L":135,"R":-132}
+        msg = {"T":1,"L":132,"R":-132}
     elif dir == 'F':
         msg = {"T":1,"L":132,"R":132}
     elif dir == 'B':
         msg = {"T":1,"L":-132,"R":-132}
-    elif dir == 'N':   
-        msg = {"T":0}
+    elif dir == 'N': 
+        msg = {"T":1,"L":0,"R":0}  
+        # msg = {"T":0}
     return msg
 
 # Input: JSON contents
@@ -149,30 +179,68 @@ def sendRoverMove(msg):
 def doTargetMove():
     print("Found Target!")
     degRange = 45 # change these if needed
-    motorRange = 132
+    motorRange = 150 # 132
+    global curr_x_limit
+    global curr_y_limit
     personDetected = True
     while(personDetected):
         # get the degree offset of the person using second line of info.txt
-        degree = float(infofile.readline().rstrip('\n'))
+        degrees = infofile.readline().rstrip('\n').split(' ')
+        x_degree = float(degrees[0])
+        y_degree = float(degrees[1])
+        print(x_degree, y_degree)
         infofile.seek(0)
-        if (abs(degree) < 2): # person is in the center! FIREEEE!
+        if (abs(x_degree) < 4 and abs(y_degree) < 1): # person is in the center! FIREEEE!
             gpio.output(17, gpio.HIGH)
+            sendRoverMove(getJSONcmd('N'))
+            sleep(0.1)
         else:
-            gpio.output(17, gpio.LOW)
-        proportion = degree / degRange # scale motor movement proportionally to degree
-        motor = round(proportion * motorRange)
-        msg = {"T":1,"L":motor,"R":-motor}
-        sendRoverMove(msg)
-        sleep(0.5)
+            if (abs(x_degree) >= 4): # fix x
+                gpio.output(17, gpio.LOW)
+                # proportion = degree / degRange # scale motor movement proportionally to degree
+                # motor = round(proportion * motorRange)
+                if (x_degree > 0): motor_step = -1
+                else: motor_step = 1
+                print("motor_step: " + str(motor_step) + " curr_x_limit: " + str(curr_x_limit))
+                if (curr_x_limit + motor_step <= x_right_limit): # goes past x_right_limit!
+                    motor_step = x_right_limit - curr_x_limit
+                elif (curr_x_limit + motor_step >= x_left_limit): # goes past x_left_limit!
+                    motor_step = x_left_limit - curr_x_limit
+                curr_x_limit += motor_step
+                focuser.set(Focuser.OPT_MOTOR_X,focuser.get(Focuser.OPT_MOTOR_X) + motor_step)
+                sleep(1)
+                # if (x_degree > 0): rover_motor = 40 
+                # else: rover_motor = -40
+                # print("rover_motor: " + str(rover_motor))
+                # msg = {"T":1,"L":rover_motor,"R":-rover_motor}
+                # sendRoverMove(msg)
+                # sleep(0.5)
+                # sendRoverMove(getJSONcmd('N'))
+                # sleep(0.8)
+            if (abs(y_degree) >= 1): # fix y
+                gpio.output(17, gpio.LOW)
+                if (y_degree > 0): motor_step = -1
+                else: motor_step = 1
+                print("motor_step: " + str(motor_step) + " curr_y_limit: " + str(curr_y_limit))
+                if (curr_y_limit + motor_step <= y_limit): # goes past y_limit!
+                    motor_step = y_limit - curr_y_limit
+                curr_y_limit += motor_step
+                focuser.set(Focuser.OPT_MOTOR_Y,focuser.get(Focuser.OPT_MOTOR_Y) + motor_step)
+                sleep(1)
+        #sleep(0.1)
         personDetected = checkForPerson()
     # what to do if person not found anymore??????
-    sendRoverMove(getJSONcmd('N'))
+    print("Lost the person! :(")
     infofile.seek(0)
+    gpio.output(17, gpio.LOW)
+    sendRoverMove(getJSONcmd('N'))
+    # sleep(0.1)
+    doMove()
 
 def doMove():
     personDetected = False
     num_creeps = 1_000_000 # CHANGE THIS
-    movementPath = ['F','F','F','F','F']
+    movementPath = ['F']
     for _ in range(num_creeps): 
         for dir in movementPath:
             personDetected = checkForPerson()
@@ -180,9 +248,14 @@ def doMove():
                 break
             infofile.seek(0)
             sendRoverMove(getJSONcmd(dir))
-            sleep(0.5)
+            #sleep(0.1)
         if personDetected: # person found!
             sendRoverMove(getJSONcmd('N'))
+            #sleep(0.1)
+            global seenFirstTime
+            if (not seenFirstTime):
+                sleep(0.6) 
+                seenFirstTime = True
             doTargetMove()
             break 
 
@@ -195,9 +268,14 @@ def main():
     ser.rts = False
     ser.dtr = False
     global infofile
+    infofile = open("info.txt", "w")
+    infofile.write("Person Not Found")
+    infofile.close()
     infofile = open("info.txt", "r")
     gpio.setmode(gpio.BCM)
     gpio.setup(17, gpio.OUT)
+    global seenFirstTime
+    seenFirstTime = False
 
     # streaming
     global picam2
@@ -206,14 +284,46 @@ def main():
     global output
     output = StreamingOutput()
 
+    # PTZ
+    global focuser
+    focuser = Focuser(1)
+    # for x: 0 is right, 90 is middle, 180 is left
+    global x_right_limit
+    x_right_limit = 0
+    global x_left_limit
+    x_left_limit = 180
+    global curr_x_limit
+    curr_x_limit = focuser.get(Focuser.OPT_MOTOR_X) # should be 90 at first
+    # for y: 0 is bottom, 90 is up
+    global y_limit
+    y_limit = 0
+    global curr_y_limit
+    curr_y_limit = focuser.get(Focuser.OPT_MOTOR_Y) # should be 90 at first
+
+    # location
+
+    # main code
     try:
+        motor_step = -75
+        curr_y_limit += motor_step
+        focuser.set(Focuser.OPT_MOTOR_Y,focuser.get(Focuser.OPT_MOTOR_Y) + motor_step)
         move_thread = threading.Thread(target=doMove)
         move_thread.daemon = True
         move_thread.start()
+        loc_thread = threading.Thread(target=doLocation)
+        loc_thread.daemon = True
+        loc_thread.start()
         doStreaming()
     finally:
+        sendRoverMove(getJSONcmd('N'))
+        motor_step = 90 - curr_y_limit
+        focuser.set(Focuser.OPT_MOTOR_Y,focuser.get(Focuser.OPT_MOTOR_Y) + motor_step)
+        sleep(0.1)
+        motor_step = 90 - curr_x_limit
+        focuser.set(Focuser.OPT_MOTOR_X,focuser.get(Focuser.OPT_MOTOR_X) + motor_step)
         ser.close()
         infofile.close()
+        gpio.output(17, gpio.LOW)
         gpio.cleanup()
         picam2.stop_recording()
 
